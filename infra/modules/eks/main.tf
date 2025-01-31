fedf4a2a4466bb842b918196404f1831edeba6b7
shivajikhot <shivajkv@gmail.com>
2025-01-31 14:52:12 +0530

    Update main.tf

--- infra/modules/eks/main.tf
+++ infra/modules/eks/main.tf
@@ -3,7 +3,7 @@ resource "aws_eks_cluster" "main" {
   name     = var.cluster_name
   role_arn = var.cluster_role_arn
   vpc_config {
-    subnet_ids = var.subnet_ids
+    subnet_ids = var.private_subnet_id
     endpoint_public_access = true
     endpoint_private_access = true
   }
