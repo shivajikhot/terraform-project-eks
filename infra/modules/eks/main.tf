e15a93cc3633b6e8ba7a5f4f7824f72cb3714853
shivajikhot <shivajkv@gmail.com>
2025-01-31 12:16:57 +0530

    Update main.tf

--- infra/modules/eks/main.tf
+++ infra/modules/eks/main.tf
@@ -28,15 +28,4 @@ resource "aws_eks_fargate_profile" "main" {
   subnet_ids = var.subnet_ids
 }
 
-# Output EKS Cluster and Fargate Profile details
-output "eks_cluster_name" {
-  value = aws_eks_cluster.main.name
-}
 
-output "eks_cluster_endpoint" {
-  value = aws_eks_cluster.main.endpoint
-}
-
-output "fargate_profile_name" {
-  value = aws_eks_fargate_profile.main.fargate_profile_name
-}
