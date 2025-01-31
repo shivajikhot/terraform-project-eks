b87e4e1fabe8b4ddc10f3a515c767bed97b064c5
shivajikhot <shivajkv@gmail.com>
2025-01-31 12:16:38 +0530

    Create variables.tf

---
+++ infra/modules/eks/variables.tf
@@ -0,0 +1,29 @@
+variable "cluster_name" {
+  type        = string
+  description = "The name of the EKS cluster."
+}
+
+variable "cluster_role_arn" {
+  type        = string
+  description = "The ARN of the EKS cluster role."
+}
+
+variable "subnet_ids" {
+  type        = list(string)
+  description = "A list of subnet IDs for the EKS cluster."
+}
+
+variable "fargate_profile_name" {
+  type        = string
+  description = "The name of the Fargate profile."
+}
+
+variable "fargate_role_arn" {
+  type        = string
+  description = "The ARN of the Fargate pod execution role."
+}
+
+variable "fargate_namespace" {
+  type        = string
+  description = "The Kubernetes namespace for Fargate."
+}
