037d87421d91d9433b07640166c9310594b1564b
shivajikhot <shivajkv@gmail.com>
2025-01-31 13:09:29 +0530

    Update main.tf

--- infra/dev/main.tf
+++ infra/dev/main.tf
@@ -42,3 +42,16 @@ output "eks_cluster_name" {
 output "eks_cluster_endpoint" {
   value = module.eks.eks_cluster_endpoint
 }
+
+
+module "ecr" {
+  source = "../../modules/ecr"
+}
+
+output "patient_service_repo_uri" {
+  value = module.ecr.patient_service_repo_uri
+}
+
+output "appointment_service_repo_uri" {
+  value = module.ecr.appointment_service_repo_uri
+}
