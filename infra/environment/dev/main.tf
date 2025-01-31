c6e1a54037e60a436653ba92b6d638eda23eee0d
shivajikhot <shivajkv@gmail.com>
2025-01-31 15:24:22 +0530

    Update main.tf

--- infra/environment/dev/main.tf
+++ infra/environment/dev/main.tf
@@ -1,8 +1,8 @@
 module "vpc" {
   source             = "../../modules/vpc"
   cidr_block         = "10.0.0.0/16"
-  public_subnet_cidr = "10.0.1.0/24"
-  private_subnet_cidr = "10.0.2.0/24"
+  public_subnet_cidrs   = ["10.0.3.0/24", "10.0.4.0/24"]
+  private_subnet_cidrs  = ["10.0.1.0/24", "10.0.2.0/24"]
   availability_zone   = ["us-west-1b", "us-west-1c"]
 }
 
