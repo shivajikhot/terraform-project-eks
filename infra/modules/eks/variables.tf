61a39513811d8bc21adb9ae1ccad90fab2bc01ab
shivajikhot <shivajkv@gmail.com>
2025-01-31 15:26:10 +0530

    Update variables.tf

--- infra/modules/eks/variables.tf
+++ infra/modules/eks/variables.tf
@@ -8,7 +8,7 @@ variable "cluster_role_arn" {
   description = "The ARN of the EKS cluster role."
 }
 
-variable "private_subnet_id" {
+variable "private_subnet_ids" {
   type        = list(string)
   description = "A list of subnet IDs for the EKS cluster."
 }
