025a76163d489c7f07d7a5a0c815e822280031d2
shivajikhot <shivajkv@gmail.com>
2025-02-02 16:57:21 +0530

    Update output.tf

--- infra/modules/iam/output.tf
+++ infra/modules/iam/output.tf
@@ -7,3 +7,10 @@ output "eks_node_role_arn" {
   description = "ARN of the IAM role for the worker nodes"
   value       = aws_iam_role.eks_node_role.arn
 }
+output "eks_worker_node_policy_attachment" {
+  value = aws_iam_role_policy_attachment.eks_worker_node
+}
+
+output "eks_cni_policy_attachment" {
+  value = aws_iam_role_policy_attachment.eks_cni_policy
+}
