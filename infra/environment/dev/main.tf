a50da768148112f20b0b42db6d6c68279d2bfdf4
shivajikhot <shivajkv@gmail.com>
2025-02-02 16:40:26 +0530

    Update main.tf

--- infra/environment/dev/main.tf
+++ infra/environment/dev/main.tf
@@ -27,6 +27,7 @@ module "eks" {
   source               = "../../modules/eks"
   cluster_name         = "my-eks-cluster"
   eks_cluster_role_arn  = module.iam.eks_cluster_role_arn
+  eks_node_role_arn  = module.iam.eks_node_role_arn 
   private_subnet_ids          = module.vpc.private_subnet_ids
 }
 
