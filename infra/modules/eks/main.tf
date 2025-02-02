9debb0f2ec23a1d06132b9ac1e503fa7c34aea5e
shivajikhot <shivajkv@gmail.com>
2025-02-02 18:13:47 +0530

    Update main.tf

--- infra/modules/eks/main.tf
+++ infra/modules/eks/main.tf
@@ -1,8 +1,57 @@
+resource "aws_security_group" "eks_sg" {
+  name        = "eks-cluster-sg"
+  description = "EKS cluster security group"
+  vpc_id      = var.vpc_id
+
+  egress {
+    from_port   = 0          # Allow all ports for egress traffic
+    to_port     = 0          # Allow all ports for egress traffic
+    protocol    = "-1"       # All protocols
+    cidr_blocks = ["0.0.0.0/0"]  # Allow traffic to anywhere
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow HTTP traffic from anywhere
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow HTTPS traffic from anywhere
+  }
+  
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow HTTPS traffic from anywhere
+  }
+
+  ingress {
+    from_port   = 3001
+    to_port     = 3001
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow HTTPS traffic from anywhere
+  }
+
+  tags = {
+    Name = "eks-cluster-sg"
+  }
+}
+
+
+
+
 resource "aws_eks_cluster" "main" {
   name     = var.cluster_name
   role_arn = var.eks_cluster_role_arn
   vpc_config {
     subnet_ids              = var.private_subnet_ids
+    security_group_ids = [aws_security_group.eks_sg.id]
     endpoint_public_access  = true
     endpoint_private_access = true
   }
