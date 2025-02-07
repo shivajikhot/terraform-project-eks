9d3a5ff996431b934e046d0e5eff6948192e9d38
shivajikhot <shivajkv@gmail.com>
2025-02-07 10:36:51 +0530

    Update output.tf

--- infra/modules/eks/output.tf
+++ infra/modules/eks/output.tf
@@ -6,3 +6,15 @@ output "eks_cluster_name" {
 output "eks_cluster_endpoint" {
   value = aws_eks_cluster.main.endpoint
 }
+
+output "eks_cluster_id" {
+  value = aws_eks_cluster.main.id
+}
+
+output "eks_cluster_certificate_authority" {
+  value = aws_eks_cluster.main.certificate_authority[0].data
+}
+
+output "eks_cluster_iam_role_arn" {
+  value = aws_eks_cluster.main.role_arn
+}
