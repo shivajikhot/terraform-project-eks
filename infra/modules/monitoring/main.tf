18ef22d0c447da9b1879bb40d3a682525931c8e0
shivajikhot <shivajkv@gmail.com>
2025-02-07 12:57:07 +0530

    Update main.tf

--- infra/modules/monitoring/main.tf
+++ infra/modules/monitoring/main.tf
@@ -32,6 +32,17 @@ resource "helm_release" "prometheus" {
   repository = "https://prometheus-community.github.io/helm-charts"
   chart      = "prometheus"
   namespace  = "monitoring"
+  timeout = 2000
+  
+  set {
+    name  = "podSecurityPolicy.enabled"
+    value = true
+  }
+
+  set {
+    name  = "server.persistentVolume.enabled"
+    value = false
+  }
 
   set {
     name  = "server.service.type"
