e1ba4db24bf2429e9f90078f10aec9d5f20945aa
shivajikhot <shivajkv@gmail.com>
2025-02-07 10:26:32 +0530

    Create main.tf

---
+++ infra/modules/monitoring/main.tf
@@ -0,0 +1,28 @@
+resource "helm_release" "prometheus" {
+  name       = "prometheus"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "prometheus"
+  namespace  = "monitoring"
+
+  set {
+    name  = "server.service.type"
+    value = "LoadBalancer"
+  }
+}
+
+resource "helm_release" "grafana" {
+  name       = "grafana"
+  repository = "https://grafana.github.io/helm-charts"
+  chart      = "grafana"
+  namespace  = "monitoring"
+
+  set {
+    name  = "service.type"
+    value = "LoadBalancer"
+  }
+
+  set {
+    name  = "adminPassword"
+    value = "grafana-admin-password"
+  }
+}
