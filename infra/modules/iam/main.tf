3cc941bb0aff0e20d9b230575e67aba8dc8709db
shivajikhot <shivajkv@gmail.com>
2025-02-07 16:52:48 +0530

    Update main.tf

--- infra/modules/iam/main.tf
+++ infra/modules/iam/main.tf
@@ -20,7 +20,7 @@ resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
-resource "aws_iam_role_policy_attachment" "ec2_container_registry" {
+resource "aws_iam_role_policy_attachment" "EBSCSIDriverPolic" {
   role       = aws_iam_role.eks_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
 }
