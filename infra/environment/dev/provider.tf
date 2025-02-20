febe9bf938d51755ad9105688eb4018da80feeff
shivajikhot <shivajkv@gmail.com>
2025-02-21 01:55:19 +0530

    Update provider.tf

--- infra/environment/dev/provider.tf
+++ infra/environment/dev/provider.tf
@@ -3,17 +3,4 @@ provider "aws" {
 }
 
 
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  token                  = module.eks.cluster_auth_token
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = module.eks.cluster_endpoint
-    token                  = module.eks.cluster_auth_token
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  }
-}
 
