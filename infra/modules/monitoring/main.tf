0f546e0b4cf6b9d045d6a1f620985d8f9cf8437f
shivajikhot <shivajkv@gmail.com>
2025-02-20 23:37:31 +0530

    Update main.tf

--- infra/modules/monitoring/main.tf
+++ infra/modules/monitoring/main.tf
@@ -1,25 +1,3 @@
-provider "kubernetes" {
-  host                   = var.eks_cluster_endpoint
-  cluster_ca_certificate = base64decode(var.eks_cluster_certificate_authority)
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    args        = ["eks", "get-token", "--cluster-name", var.eks_cluster_id]
-  }
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = var.eks_cluster_endpoint
-    cluster_ca_certificate = base64decode(var.eks_cluster_certificate_authority)
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      command     = "aws"
-      args        = ["eks", "get-token", "--cluster-name", var.eks_cluster_id]
-    }
-  }
-}
-
 # Create Namespace for Monitoring
 resource "kubernetes_namespace" "monitoring" {
   metadata {
