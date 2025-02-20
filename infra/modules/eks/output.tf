bdd080d052eb2cc85c5f33ecd9427107d7d27454
shivajikhot <shivajkv@gmail.com>
2025-02-21 01:13:43 +0530

    Update output.tf

--- infra/modules/eks/output.tf
+++ infra/modules/eks/output.tf
@@ -18,3 +18,16 @@ output "eks_cluster_certificate_authority" {
 output "eks_cluster_iam_role_arn" {
   value = aws_eks_cluster.main.role_arn
 }
+
+output "cluster_endpoint" {
+  value = aws_eks_cluster.main.endpoint
+}
+
+output "cluster_auth_token" {
+  value = data.aws_eks_cluster_auth.cluster.token
+}
+
+output "cluster_certificate_authority_data" {
+  value = aws_eks_cluster.main.certificate_authority[0].data
+}
+
