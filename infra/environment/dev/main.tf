46ce91c5b614070005618aa40951a081229c30f4
shivajikhot <shivajkv@gmail.com>
2025-02-21 00:12:40 +0530

    Update main.tf

--- infra/environment/dev/main.tf
+++ infra/environment/dev/main.tf
@@ -27,8 +27,5 @@ module "ecr" {
 
 module "monitoring" {
   source                              = "../../modules/monitoring"
-  eks_cluster_id                      = module.eks.eks_cluster_id
-  eks_cluster_endpoint                 = module.eks.eks_cluster_endpoint
-  eks_cluster_certificate_authority    = module.eks.eks_cluster_certificate_authority
   depends_on = [module.eks]
 }
