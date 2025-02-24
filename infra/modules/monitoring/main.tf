6e437d8c70e51850c6bec6edb4335a0f7e23f013
shivajikhot <shivajkv@gmail.com>
2025-02-24 17:58:46 +0530

    Update main.tf

--- infra/modules/monitoring/main.tf
+++ infra/modules/monitoring/main.tf
@@ -30,7 +30,7 @@ resource "kubernetes_namespace" "monitoring" {
 resource "helm_release" "prometheus" {
   name       = "prometheus"
   repository = "https://prometheus-community.github.io/helm-charts"
-  chart      = "prometheus"
+  chart      = "kube-prometheus-stack"
   namespace  = "monitoring"
   timeout = 200
   set {
