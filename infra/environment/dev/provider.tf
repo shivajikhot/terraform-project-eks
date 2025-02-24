bd55f514cd5a9c1cfa5db3bf541f1d0475fcf53c
shivajikhot <shivajkv@gmail.com>
2025-02-24 18:15:57 +0530

    Update provider.tf

--- infra/environment/dev/provider.tf
+++ infra/environment/dev/provider.tf
@@ -2,5 +2,18 @@ provider "aws" {
   region = "us-west-1"
 }
 
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  token                  = module.eks.cluster_auth_token
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    token                  = module.eks.cluster_auth_token
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  }
+}
 
 
